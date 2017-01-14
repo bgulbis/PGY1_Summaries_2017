ab08ffb77dfaa9f7420b5b0a2c791bdd4cd97865
bgulbis <brian.gulbis@gmail.com>
2017-01-14 16:11:10 -0600

    add script to make applicant summary

---
+++ src/2-create_applicant_summaries.R
@@ -0,0 +1,14 @@
+# create applicant summaries
+
+library(tidyverse)
+library(stringr)
+library(ReporteRs)
+
+dirr::get_rds("data/tidy")
+
+save_dir <- "report/summaries"
+
+# for (i in 1:nrow(data_applicants)) {
+for (i in 1:5) {
+
+}
