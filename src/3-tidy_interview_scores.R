834233e5d7bec325482186689fc43d439048f451
bgulbis <brian.gulbis@gmail.com>
2017-02-27 10:24:46 -0600

    summarize interview scores and export results

--- src/3-tidy_interview_scores.R
+++ src/3-tidy_interview_scores.R
@@ -20,12 +20,12 @@ interview_remarks <- raw_scores %>%
     group_by(cas_id, interviewer) %>%
     summarize_at("score", mean, na.rm = TRUE) %>%
     spread(interviewer, score) %>%
-    mutate(fit_mean = mean(mmi_ec, mmi_to, preceptor, residents, rpd),
-           fit_total = sum(mmi_ec, mmi_to, preceptor, residents, rpd))
+    mutate(interview_fit_mean = mean(mmi_ec, mmi_to, preceptor, residents, rpd),
+           interview_fit_total = sum(mmi_ec, mmi_to, preceptor, residents, rpd))
 
 # interview scores -------------------------------------
 
-interview_questions <- raw_scores %>%
+interview_results <- raw_scores %>%
     select(cas_id, contains("score")) %>%
     select(cas_id, contains("question")) %>%
     gather(interviewer, score, -cas_id) %>%
@@ -40,9 +40,29 @@ interview_questions <- raw_scores %>%
     dmap_at("interviewer", str_replace_all, pattern = "_(short_deadline|unclear_expectations)", replacement = "B") %>%
     dmap_at("interviewer", str_replace_all, pattern = "(critical|new_drug_disease_state|stressful_situation)_", replacement = "") %>%
     group_by(cas_id, interviewer) %>%
-    summarize_at("score", mean, na.rm = TRUE) %>%
-    spread(interviewer, score) %>%
-    mutate(fit_mean = mean(mmi_ec, mmi_to, preceptor, residents, rpd),
-           fit_total = sum(mmi_ec, mmi_to, preceptor, residents, rpd))
+    summarize_at("score", mean, na.rm = TRUE)
 
+interview_questions <- spread(interview_results, interviewer, score)
 
+interview_totals <- interview_results %>%
+    group_by(cas_id) %>%
+    summarize_at("score", funs(mean, sum)) %>%
+    rename(interview_mean = mean,
+           interview_total = sum)
+
+results_interviews <- interview_totals %>%
+    left_join(interview_questions, by = "cas_id") %>%
+    left_join(interview_remarks, by = "cas_id") %>%
+    left_join(results_application, by = "cas_id") %>%
+    select(cas_id, last_name:school, interview_mean, interview_total,
+           interview_fit_mean, interview_fit_total, score_application,
+           score_vidyo, remark_vidyo, interest_1, interest_2, interest_3,
+           everything()) %>%
+    arrange(desc(interview_fit_total), desc(interview_total))
+
+results_summary <- select(results_interviews, cas_id:interest_3)
+
+write_rds(results_interviews, "data/final/results_interviews.rds", "gz")
+write_csv(results_interviews, "data/final/results_interviews.csv")
+write_rds(results_summary, "data/final/results_summary.rds", "gz")
+write_csv(results_summary, "data/final/results_summary.csv")
